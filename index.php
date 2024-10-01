28a496b956e61614f6a0ca0d9807ddb9b3950cb2
BlazejMistak <blazej.mistak.bm@gmail.com>
2024-10-01 20:25:33 +0200

    secuirty of entering (-) number

--- index.php
+++ index.php
@@ -34,6 +34,22 @@
                 <input type="number" name="PLN1gr" placeholder="0,01 PLN"><br>
                 <input type="submit" value="submit">
             </form>
+<?php 
+    // setting variables value to null.
+    $PLN500 = null;$PLN200 = null;$PLN100 = null;$PLN50 = null;$PLN20 = null;$PLN10 = null;$PLN5 = null;$PLN2 = null;$PLN1 = null;$PLN50gr = null;$PLN20gr = null;$PLN10gr = null;$PLN5gr = null;$PLN2gr = null;$PLN1gr = null;
+    // setting variables value to user input.
+    $PLN500 = (int) $_POST['PLN500']; $PLN200 = (int) $_POST['PLN200'];$PLN100 = (int)$_POST['PLN100'];$PLN50 = (int)$_POST['PLN50'];$PLN20 = (int)$_POST['PLN20'];$PLN10 =(int) $_POST['PLN10'];$PLN5 = (int)$_POST['PLN5'];$PLN2 = (int)$_POST['PLN2'];$PLN1 = (int)$_POST['PLN1'];$PLN50gr =(int) $_POST['PLN50gr'];$PLN20gr =(int) $_POST['PLN20gr'];$PLN20gr = (int)$_POST['PLN20gr'];$PLN10gr = (int)$_POST['PLN10gr'];$PLN5gr = (int)$_POST['PLN5gr'];$PLN2gr = (int)$_POST['PLN2gr'];$PLN1gr = (int)$_POST['PLN1gr'];
+    //checking if user input is not negative
+    if($PLN500<0 | $PLN200<0 | $PLN100<0 | $PLN50<0 | $PLN20<0 | $PLN10<0 | $PLN5<0 | $PLN2<0 | $PLN1<0 | $PLN50gr<0 | $PLN20gr<0 | $PLN10gr<0 | $PLN5gr<0 | $PLN2gr<0 | $PLN1gr<0){
+        echo "Wprowadzono nieprawidłową liczbę banknotów/monet. Liczba banknotów/monet nie może być mniejsza od 0.";
+    }
+    else{
+    //calculating total sum of money
+    $PLNsum = null;
+    $PLNsum = $PLN500* 500 + $PLN200* 200 + $PLN100* 100 + $PLN50* 50 + $PLN20* 20 + $PLN10* 10 + $PLN5* 5 + $PLN2* 2 + $PLN1* 1 + $PLN50gr* 0.5 + $PLN20gr* 0.2 + $PLN10gr* 0.1 + $PLN5gr* 0.05 + $PLN2gr* 0.02 + $PLN1gr* 0.01;
+    echo "Suma wynosi {$PLNsum} PLN. <br>";
+    }
+?>
         </section>
     </main>
     <aside></aside>
@@ -43,13 +59,3 @@
 </body>
 </html>
 
-<?php 
-    // setting variables value to null.
-    $PLN500 = null;$PLN200 = null;$PLN100 = null;$PLN50 = null;$PLN20 = null;$PLN10 = null;$PLN5 = null;$PLN2 = null;$PLN1 = null;$PLN50gr = null;$PLN20gr = null;$PLN10gr = null;$PLN5gr = null;$PLN2gr = null;$PLN1gr = null;
-    // setting variables value to user input.
-    $PLN500 = (int) $_POST['PLN500']; $PLN200 = (int) $_POST['PLN200'];$PLN100 = (int)$_POST['PLN100'];$PLN50 = (int)$_POST['PLN50'];$PLN20 = (int)$_POST['PLN20'];$PLN10 =(int) $_POST['PLN10'];$PLN5 = (int)$_POST['PLN5'];$PLN2 = (int)$_POST['PLN2'];$PLN1 = (int)$_POST['PLN1'];$PLN50gr =(int) $_POST['PLN50gr'];$PLN20gr =(int) $_POST['PLN20gr'];$PLN20gr = (int)$_POST['PLN20gr'];$PLN10gr = (int)$_POST['PLN10gr'];$PLN5gr = (int)$_POST['PLN5gr'];$PLN2gr = (int)$_POST['PLN2gr'];$PLN1gr = (int)$_POST['PLN1gr'];
-    //calculating total sum of money
-    $PLNsum = null;
-    $PLNsum = $PLN500* 500 + $PLN200* 200 + $PLN100* 100 + $PLN50* 50 + $PLN20* 20 + $PLN10* 10 + $PLN5* 5 + $PLN2* 2 + $PLN1* 1 + $PLN50gr* 0.5 + $PLN20gr* 0.2 + $PLN10gr* 0.1 + $PLN5gr* 0.05 + $PLN2gr* 0.02 + $PLN1gr* 0.01;
-    echo "Suma wynosi {$PLNsum} PLN. <br>";
-?>
