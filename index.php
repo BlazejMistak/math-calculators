00431e16e3f173dc374395e7d62187416a741568
BlazejMistak <blazej.mistak.bm@gmail.com>
2024-10-01 19:01:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Błażej Miśtak">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <h1>money calculator</h1>
+    </nav>
+    <main>
+        <section class="money_usage">
+            <span>Money calculator</span>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita, ullam reprehenderit maiores laboriosam quibusdam vitae excepturi delectus quisquam magnam? Magnam consequuntur cupiditate cum vitae, odit corporis possimus dicta laudantium ipsam!</p>
+        </section>
+        <section class="money_calculator"></section>
+    </main>
+    <aside></aside>
+    <footer>
+        <span>Author: Błażej Miśtak</span>
+    </footer>
+</body>
+</html>
